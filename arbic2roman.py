d9de3f96ed753d628a69328ac672ca220d9ed04e
GreenyWind <whelzyq@163.com>
2020-01-27 10:23:02 -0500

    convert arbic numerals to roman numerals

---
+++ arbic2roman.py
@@ -0,0 +1,25 @@
+romans = [["", "M", "MM", "MMM"]
+        , ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+        , ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+    , ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]]
+
+def a2r(input_):
+    retVal = ""
+    if isinstance(input_, int):
+        if input_ <= 0:
+            retVal = "invalid input: not a positive integer"
+        elif input_ > 3999:
+            retVal = "invalid input: integer out of range [1, 3999]"
+        else:
+            num = input_
+            for i in range(4):
+                digit = num % 10
+                retVal += romans[i][digit]
+                num = num // 10
+    else:
+        if isinstance(input_, float):
+            retVal = "invalid input: not an integer"
+        else:
+            retVal = "invalid input: not a number"
+
+    return retVal
