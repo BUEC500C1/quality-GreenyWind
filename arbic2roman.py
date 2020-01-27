3fc75ca6debdeb487ea6a2a9791f565e2f91a792
GreenyWind <whelzyq@163.com>
2020-01-27 10:29:22 -0500

    convert arbic numerals to roman numerals

--- arbic2roman.py
+++ arbic2roman.py
@@ -1,5 +1,4 @@
-romans = [   
-    , ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+romans = [["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
     , ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
     , ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
     , ["", "M", "MM", "MMM"]
